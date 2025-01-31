9ddaa6a2372a306fb4fb3eb08e4c5950e3616206
Amir <rasoulkhalkhali999@gmail.com>
2025-01-31 05:56:18 +0330

    Update Wp2.py

--- Wp2.py
+++ Wp2.py
@@ -1,56 +1,52 @@
-import ipaddress, platform, subprocess, os, datetime, base64
+import ipaddress, platform, subprocess, os, datetime, base64, json
 
 warp_cidr = [
-    "188.114.96.0/24",
-    "188.114.97.0/24",
-    "188.114.98.0/24",
-    "188.114.99.0/24",
-    "162.159.192.0/24",
-    "162.159.193.0/24",
-    "162.159.195.0/24",
-    "162.159.204.0/24",
-]
+        '162.159.192.0/24',
+        '162.159.193.0/24',
+        '162.159.195.0/24',
+        '162.159.204.0/24',
+        '188.114.96.0/24',
+        '188.114.97.0/24',
+        '188.114.98.0/24',
+        '188.114.99.0/24'
+    ]
 
 script_directory = os.path.dirname(__file__)
-cfw_ips_txt_path = os.path.join(script_directory, "cfw-ips.txt")
-result_path = os.path.join(script_directory, "result.csv")
-
+ip_txt_path = os.path.join(script_directory, 'ip.txt')
+result_path = os.path.join(script_directory, 'result.csv')
 
 def create_ips():
     c = 0
     total_ips = sum(len(list(ipaddress.IPv4Network(cidr))) for cidr in warp_cidr)
 
-    with open(cfw_ips_txt_path, "w") as file:
+    with open(ip_txt_path, 'w') as file:
         for cidr in warp_cidr:
             ip_addresses = list(ipaddress.IPv4Network(cidr))
             for addr in ip_addresses:
                 c += 1
                 file.write(str(addr))
                 if c != total_ips:
-                    file.write("\n")
+                    file.write('\n')
 
-
-if os.path.exists(cfw_ips_txt_path):
-    print("cfw-ips.txt exist.")
+if os.path.exists(ip_txt_path):
+    print("ip.txt exist.")
 else:
-    print("Creating cfw-ips.txt File.")
+    print('Creating ip.txt File.')
     create_ips()
-    print("cfw-ips.txt File Created Successfully!")
-
+    print('ip.txt File Created Successfully!')
 
 def arch_suffix():
     machine = platform.machine().lower()
-    if machine.startswith("i386") or machine.startswith("i686"):
-        return "386"
-    elif machine.startswith(("x86_64", "amd64")):
-        return "amd64"
-    elif machine.startswith(("armv8", "arm64", "aarch64")):
-        return "arm64"
-    elif machine.startswith("s390x"):
-        return "s390x"
+    if machine.startswith('i386') or machine.startswith('i686'):
+        return '386'
+    elif machine.startswith(('x86_64', 'amd64')):
+        return 'amd64'
+    elif machine.startswith(('armv8', 'arm64', 'aarch64')):
+        return 'arm64'
+    elif machine.startswith('s390x'):
+        return 's390x'
     else:
         raise ValueError("Unsupported CPU architecture")
-
 
 arch = arch_suffix()
 
@@ -71,45 +67,28 @@ if process.returncode != 0:
 else:
     print("Warp executed successfully.")
 
-Bestip = []
-
-with open(result_path, "r") as csv_file:
-    next(csv_file)
-    c = 0
-    for line in csv_file:
-        Bestip.append(line.split(",")[0])
-        c += 1
-        if c == 2:
-            break
-
-
 def warp_ip():
+    counter = 0
+    config_prefixes = ""
     creation_time = os.path.getctime(result_path)
-    formatted_time = datetime.datetime.fromtimestamp(creation_time).strftime(
-        "%Y-%m-%d %H:%M:%S"
-    )
-    for i, ip in enumerate(Bestip):
-        config_prefix = f"warp://{Bestip[0]}?ifp=1-3&ifpm=m4#@arshiacomplus🇮🇷&&detour=warp://{Bestip[1]}?ifp=1-3&ifpm=m6#arshiacomplus🇩🇪WoW\nwarp://{Bestip[0]}?ifp=2-4&ifpm=m3#@arshiacomplus🇮🇷&&detour=warp://{Bestip[1]}?ifp=1-3&ifpm=m6#arshiacomplus🇩🇪WoW\nwarp://@auto6/?ifp=1-3&ifpm=m4#LocalV6&&detour=warp://@auto4/?ifp=1-3&ifpm=m6#WoWV4_v6"
-    return config_prefix, formatted_time
+    formatted_time = datetime.datetime.fromtimestamp(creation_time).strftime("%Y-%m-%d %H:%M:%S")
+    with open(result_path, 'r') as csv_file:
+        next(csv_file)
+        for ips in csv_file:
+            counter += 1
+            if counter == 5:
+                break
+            else:
+                ip = ips.split(',')[0]
+                config_prefix = f'\nwarp://{ip}?ifp=1-3&ifpm=m4\n'
+                config_prefixes += config_prefix
+    return config_prefixes, formatted_time
 
 
-title = (
-    "//profile-title: base64:"
-    + base64.b64encode("Warp2".encode("utf-8")).decode("utf-8")
-    + "\n"
-)
-update_interval = "//profile-update-interval: 1\n"
-sub_info = "//subscription-userinfo: upload=0; download=0; total=10737418240000000; expire=2546249531\n"
-profile_web = "//profile-web-page-url: https://github.com/mansor427\n"
-last_modified = "//last update on: " + warp_ip()[1] + "\n"
 configs = warp_ip()[0]
-with open("warp2.json", "w") as op:
-    op.write(title + update_interval + sub_info + profile_web + last_modified + configs)
+with open('warpauto.json', 'w') as op:
+    op.write(configs)
 
-with open("Bestip2.txt", "w") as f:
-    for ip in Bestip:
-        f.write(f"{ip}\n")
-
-os.remove(cfw_ips_txt_path)
+os.remove(ip_txt_path)
 os.remove(result_path)
 os.remove("warp")
